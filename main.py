aaec1a27d0f6f2e4077f5da449174e36915d5f7f
Francisco Arley Coelho Barros <contatofcarley@gmail.com>
2023-05-11 20:46:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 """Main functions"""
-
+import re
 
 def is_palindrome(string: str) -> bool:
-    """Check if string is palindrome."""
-    return False
+    td = string.lower()
+    a1 = re.sub("[.,! s?@:;]", "", td)
+    string_invertida = a1[::-1]
+    if a1 == string_invertida:
+        return True
+    else:
+        return False
+
